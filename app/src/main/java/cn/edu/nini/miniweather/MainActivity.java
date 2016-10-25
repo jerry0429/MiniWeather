7b857e6ac649d838e3caa4b4efffe73b845508fa
nijun0718 <824291045@qq.com>
2016-10-25 15:56:54 +0800

    init project

---
+++ app/src/main/java/cn/edu/nini/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.nini.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nini on 2016/10/25.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
